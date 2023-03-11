d37207b4cb0a3f18d767b790ee06f08dfcafd49e
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-11 18:01:49 +0300

    Create skeleton of page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello");
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.getInfo = function () {
+  const info = `${title} by ${author}, ${pages} pages, ${read}`;
+  return info;
+};
